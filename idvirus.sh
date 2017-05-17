63f0e25c45b7a337bf1408abbf1604e4a03bb057
stuber <tod.p.stuber@usda.gov>
2017-05-17 06:33:57 -0500

    wip

--- idvirus.sh
+++ idvirus.sh
@@ -16,7 +16,7 @@ mydb="/data/mydb"
 dzdo chmod 755 * ${mydb}/*
 for i in ${mydb}/*; do
 	if [ -s $i ]; then
-        	echo "file $i exists" 2> /dev/null
+        	echo "file $i exists" > /dev/null # don't print to screen
 	else
         	echo "file $i is empty and has been deleted"
 		rm -f $i
