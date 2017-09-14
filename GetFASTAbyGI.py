e4446635d0860458aab91d8c8d97f97b3dcefd3e
stuber <tstuber@aapwima5vsmalex.we.aphis.gov>
2017-09-14 06:25:04 -0500

    add dependent script back, GetFASTAbyGI.py

---
+++ GetFASTAbyGI.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from Bio import SeqIO
+from Bio import Entrez
+from sys import argv
+
+#provide a GI or accession number
+script, acc, writeFile = argv
+
+#print ("This is the GI number: %s " % gi)
+#print ("This is the file to write too: %s " % writeFile)
+#print ("script: %s " % script)
+
+entrezDbName = 'nucleotide'
+Entrez.email = 'tod.p.stuber@usda.gov'
+
+entryData = Entrez.efetch(db=entrezDbName, id=gi, retmode="text", rettype='fasta')
+
+local_file=open(writeFile,"w")
+local_file.write(entryData.read())
+entryData.close()
+local_file.close()
+
